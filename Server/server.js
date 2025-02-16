7ef90093faddd2851bf0a2fe3244f5b4e5741df6
AllergyOat <apichat.peanroop@gmail.com>
2025-02-17 00:14:20 +0700

    Connect to MySQL database and download JWT packages

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const mysql = require('mysql2');
 const app = express();
 const port = 5000;
 
-
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
+
+
+//Mysql Database Connection XD
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '473625',
+    database: 'dsldb'
+});
+db.connect(err => { if (err) throw err; console.log('MySQL Connected'); });
+
+db.query('SELECT * FROM student', (err, results) => {
+    if (err) {
+        console.error('Database query error:', err);
+    } else {
+        console.log('Admin Data:', results);
+    }
+});
+
 
 
 app.get('/api', (req, res) => {

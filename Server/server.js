2078081a9ca0155bcb6e295e628749992b5c9164
AllergyOat <apichat.peanroop@gmail.com>
2025-02-19 21:21:45 +0700

    Implemented JWT authentication with login & register

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-
 //Mysql Database Connection XD
 const db = mysql.createConnection({
     host: 'localhost',
@@ -18,26 +17,18 @@ const db = mysql.createConnection({
     database: 'dsldb'
 });
 db.connect(err => { if (err) throw err; console.log('MySQL Connected'); });
+module.exports = db;
 
-db.query('SELECT * FROM student', (err, results) => {
+db.query('SELECT * FROM user', (err, results) => {
     if (err) {
         console.error('Database query error:', err);
     } else {
-        console.log('Admin Data:', results);
+        console.log('user Data:', results.map(user => user.name));
     }
 });
 
-
-
-app.get('/api', (req, res) => {
-    res.send("Hello World!");
-})
-
-app.post('/api', (req, res) => {
-    console.log(req.body);
-    res.send("Post request received!");
-})
-
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
